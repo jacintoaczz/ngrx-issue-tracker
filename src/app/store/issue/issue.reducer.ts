ae498be82b3d3915f593e3aab8e63b58ebee1f3a
Jacinto Acosta <jacintoaczz@gmail.com>
2023-06-17 12:38:24 -0400

    Register the reducer that will take care of the submit action processing

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,5 +1,18 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 
-import { initialState } from './issue.state';
+import { initialState, IssueState } from './issue.state';
+import * as IssueActions from './issue.actions';
 
-export const issuesReducer = createReducer(initialState);
+export const issuesReducer = createReducer(
+    initialState,
+    on(IssueActions.submit, (state: IssueState, { issue }) => ({
+        ...state,
+        entities: {
+            ...state.entities,
+            [issue.id]: {
+                ...issue,
+                resolved: false,
+            },
+        },
+    }))
+);
