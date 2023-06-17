47692918545ec54a435f9ab66929df4dc8373cf8
Jacinto Acosta <jacintoaczz@gmail.com>
2023-06-17 10:15:31 -0400

    Create a basic routing for the issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IssuesComponent } from '@components/issues/issues.component';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'issues' },
+    { path: 'issues', component: IssuesComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
