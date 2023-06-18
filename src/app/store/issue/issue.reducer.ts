e09e45bbebaf0ac048aef86759787593c249bbc3
Jacinto Acosta <jacintoaczz@gmail.com>
2023-06-17 21:29:22 -0400

    Add documentation

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,18 +1,20 @@
 import { createReducer, on } from '@ngrx/store';
+import { produce } from 'immer';
 
 import { initialState, IssueState } from './issue.state';
 import * as IssueActions from './issue.actions';
 
+/**
+ * Reducer in charge of processing the IssueState.
+ */
 export const issuesReducer = createReducer(
     initialState,
-    on(IssueActions.submit, (state: IssueState, { issue }) => ({
-        ...state,
-        entities: {
-            ...state.entities,
-            [issue.id]: {
+    on(IssueActions.submit, (state: IssueState, { issue }) =>
+        produce(state, (draft) => {
+            draft.entities[issue.id] = {
                 ...issue,
                 resolved: false,
-            },
-        },
-    }))
+            };
+        })
+    )
 );
