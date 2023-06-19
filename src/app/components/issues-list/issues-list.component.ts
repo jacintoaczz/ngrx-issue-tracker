9dd3f1fb1e2f0dbfc983607c217aaa9d2951ce25
Jacinto Acosta <jacintoaczz@gmail.com>
2023-06-18 21:33:17 -0400

    Add a new component for rendering the issues list
    - Bind the store to the component
    - Create a search input event handler
    - Add some styling

---
+++ src/app/components/issues-list/issues-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { Issue } from '@models/issues/issue.interface';
+import { RootState } from '@app/store';
+import * as fromIssues from '@store/issue/issue.selector';
+import * as IssueActions from '@store/issue/issue.actions';
+
+/**
+ * Component that renders the list of issues
+ */
+@Component({
+    selector: 'issues-list',
+    templateUrl: './issues-list.component.html',
+    styleUrls: ['./issues-list.component.scss'],
+})
+export class IssuesListComponent {
+    issues$: Observable<Issue[]>;
+
+    /**
+     * DI Constructor
+     * @param _store
+     */
+    constructor(private _store: Store<RootState>) {
+        this.issues$ = this._store.select(fromIssues.selectAll);
+    }
+
+    /**
+     * Search input on input event.
+     * @param text
+     */
+    search(text: string): void {
+        this._store.dispatch(IssueActions.search({ text }));
+    }
+}
