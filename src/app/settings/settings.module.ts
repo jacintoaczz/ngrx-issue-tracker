4c0084fa1569e7bc7c5f730233cd531a109c3801
Jacinto Acosta <jacintoaczz@gmail.com>
2023-07-04 13:25:22 -0400

    Implement the corresponding components/modules

---
+++ src/app/settings/settings.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
+import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsComponent } from './settings.component';
+import { settingsFeatureKey } from '@app/settings/store/settings.state';
+import { settingsReducer } from '@app/settings/store/settings.reducer';
+
+/**
+ * Settings feature module.
+ */
+@NgModule({
+    declarations: [SettingsComponent],
+    imports: [CommonModule, SettingsRoutingModule, StoreModule.forFeature(settingsFeatureKey, settingsReducer)],
+})
+export class SettingsModule {}
