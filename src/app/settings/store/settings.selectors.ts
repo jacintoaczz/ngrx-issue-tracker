9794a2e47f6bbb1923504a7e0cebb26cf49bd882
Jacinto Acosta <jacintoaczz@gmail.com>
2023-07-14 17:45:06 -0400

    refactor: Remove the deprecated implementation for createFeatureSelector()

--- src/app/settings/store/settings.selectors.ts
+++ src/app/settings/store/settings.selectors.ts
@@ -1,11 +1,11 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-import { settingsFeatureKey, SettingsRootState, SettingsState } from '@app/settings/store/settings.state';
+import { settingsFeatureKey, SettingsState } from '@app/settings/store/settings.state';
 
 /**
  * Main feature selector for the "Settings" feature.
  */
-export const selectFeature = createFeatureSelector<SettingsRootState, SettingsState>(settingsFeatureKey);
+export const selectFeature = createFeatureSelector<SettingsState>(settingsFeatureKey);
 
 /**
  * Selects the given notification priority
